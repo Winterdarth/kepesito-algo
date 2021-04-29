6a4857783a73015ecb90a830b5ee5b78056d9e73
David Kajdarov <kajdarov.d@gmail.com>
2021-04-29 11:07:39 +0200

    Finish project

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,18 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        if (input.isEmpty() || input == null) {
+            return -1;
+        }
+
+        int sum = 0;
+        for(int i = 0; i < input.length() ; i++){
+            if( Character.isDigit(input.charAt(i)) ){
+                sum = sum + Character.getNumericValue(input.charAt(i));
+            }
+        }
+        return sum;
+    }
 }
